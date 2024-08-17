7eed8a8009514924f480ded3ee379a8286b0aba8
Arthur P. Côrtes <35460423+arthurpc02@users.noreply.github.com>
2024-08-17 01:01:51 +0000

    receives data in chunks.

--- timeSeriesDataset.py
+++ timeSeriesDataset.py
@@ -1,27 +1,47 @@
 import requests
 import pandas as pd
+from io import StringIO
 
 ##########################################
 # 1. Stream Data
-stream = requests.get('https://docs.google.com/spreadsheets/d/19galjYSqCDf6Ohb0IWv6YsRL7MV0EPFpN-2blGGS97U/pub?output=csv')
-print(stream.status_code)
-print(stream.headers)
-csv = stream.content
+##########################################
+def receiveDataInChunks(desiredChunkSize=10):
+    """Simulates a stream of continuous data"""
 
-# dataset = pd.read_csv(stream)
-# print(dataset)
-# https://docs.google.com/spreadsheets/d/19galjYSqCDf6Ohb0IWv6YsRL7MV0EPFpN-2blGGS97U/pub?output=csv
+    chunk = []
+
+    url = 'https://docs.google.com/spreadsheets/d/19galjYSqCDf6Ohb0IWv6YsRL7MV0EPFpN-2blGGS97U/pub?output=csv'
+    response = requests.get(url, stream=True)
+
+    for line in response.iter_lines():
+        if line:
+            chunk.append(line.decode('utf-8'))
+        if len(chunk) >= desiredChunkSize:
+            break
+    
+    return chunk
+
+print(receiveDataInChunks())
+
+# csv = stream.content
+
+# dataset = pd.read_csv(StringIO(stream.text))
+
 
 
 ##########################################
 # 2. Anomaly Detection
-
+##########################################
 
 
 ##########################################
 # 3. Metrics Reporting
-
+##########################################
 
 
 ##########################################
 # 4. Code Submission
+##########################################
+
+# print(dataset)
+# print(dataset.info())
